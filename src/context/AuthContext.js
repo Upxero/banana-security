57e9354c02b353fbbc5c2fe6112250d1272b5ab6
Michael Bernard <104996295+Upxero@users.noreply.github.com>
2023-01-23 17:42:32 +0100

    assignment pro completed

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,33 +1,97 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import axios from 'axios';
+import isTokenValid from '../helpers/isTokenValid';
 
 export const AuthContext = createContext({});
 
 function AuthContextProvider({ children }) {
-    const [isAuth, toggleIsAuth] = useState(false);
+    const [isAuth, toggleIsAuth] = useState({
+        isAuth: false,
+        user: null,
+        status: 'pending',
+    });
     const history = useHistory();
 
-    function login() {
-        console.log('Gebruiker is ingelogd!');
-        toggleIsAuth(true);
-        history.push('/profile');
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+
+        if (token && isTokenValid(token)) {
+            const decoded = jwt_decode(token);
+            void fetchUserData(decoded.sub, token);
+        } else {
+            toggleIsAuth({
+                isAuth: false,
+                user: null,
+                status: 'done',
+            });
+        }
+    }, []);
+
+    function login(JWT) {
+        localStorage.setItem('token', JWT);
+        const decoded = jwt_decode(JWT);
+
+        void fetchUserData(decoded.sub, JWT, '/profile');
     }
 
     function logout() {
+        localStorage.clear();
+        toggleIsAuth({
+            isAuth: false,
+            user: null,
+            status: 'done',
+        });
+
         console.log('Gebruiker is uitgelogd!');
-        toggleIsAuth(false);
         history.push('/');
     }
 
+    async function fetchUserData(id, token, redirectUrl) {
+        try {
+            const result = await axios.get(`http://localhost:3000/600/users/${id}`, {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+
+            toggleIsAuth({
+                ...isAuth,
+                isAuth: true,
+                user: {
+                    username: result.data.username,
+                    email: result.data.email,
+                    id: result.data.id,
+                },
+                status: 'done',
+            });
+
+            if (redirectUrl) {
+                history.push(redirectUrl);
+            }
+
+        } catch (e) {
+            console.error(e);
+            toggleIsAuth({
+                isAuth: false,
+                user: null,
+                status: 'done',
+            });
+        }
+    }
+
     const contextData = {
-        isAuth: isAuth,
+        isAuth: isAuth.isAuth,
+        user: isAuth.user,
         login: login,
         logout: logout,
     };
 
     return (
         <AuthContext.Provider value={contextData}>
-            {children}
+            {isAuth.status === 'done' ? children : <p>Loading...</p>}
         </AuthContext.Provider>
     );
 }
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,22 +1,58 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
+import axios from 'axios';
 
 function Profile() {
-  return (
-    <>
-      <h1>Profielpagina</h1>
-      <section>
-        <h2>Gegevens</h2>
-        <p><strong>Gebruikersnaam:</strong> hardcoded-test</p>
-        <p><strong>Email:</strong> hardcoded@test.com</p>
-      </section>
-      <section>
-        <h2>Strikt geheime profiel-content</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias cum debitis dolor dolore fuga id molestias qui quo unde?</p>
-      </section>
-      <p>Terug naar de <Link to="/">Homepagina</Link></p>
-    </>
-  );
+    const [profileData, setProfileData] = useState({});
+    const { user } = useContext(AuthContext);
+
+    useEffect(() => {
+        const source = axios.CancelToken.source();
+
+        async function fetchProfileData() {
+            const token = localStorage.getItem('token');
+
+            try {
+                const result = await axios.get('http://localhost:3000/660/private-content', {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`,
+                    },
+                    cancelToken: source.token,
+                });
+
+                setProfileData(result.data);
+            } catch (e) {
+                console.error(e);
+            }
+        }
+        void fetchProfileData();
+
+        return function cleanup() {
+            source.cancel();
+        }
+    }, []);
+
+    return (
+        <>
+            <h1>Profielpagina</h1>
+            <section>
+                <h2>Gegevens</h2>
+                <p><strong>Gebruikersnaam:</strong> {user.username}</p>
+                <p><strong>Email:</strong> {user.email}</p>
+            </section>
+
+            {Object.keys(profileData).length > 0 &&
+                <section>
+                    <h2>Strikt geheime profiel-content</h2>
+                    <h3>{profileData.title}</h3>
+                    <p>{profileData.content}</p>
+                </section>
+            }
+            <p>Terug naar de <Link to="/">Homepagina</Link></p>
+        </>
+    );
 }
 
 export default Profile;
